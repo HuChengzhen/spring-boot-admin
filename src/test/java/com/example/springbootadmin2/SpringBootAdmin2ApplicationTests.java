bc46b99557566471ef6cb40f827aa6358d53cdda
huchengzhen <huchengzhen@gmail.com>
2021-01-24 09:17:40 -0800

    init

---
+++ src/test/java/com/example/springbootadmin2/SpringBootAdmin2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootadmin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdmin2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
